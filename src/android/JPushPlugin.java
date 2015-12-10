23fd3bc0cba5244aa5c4ea08517f81e71c791cc4
darkterrorooo <rprpftj@163.com>
2015-12-11 00:38:48 +0800

    fix #72 #99

--- src/android/JPushPlugin.java
+++ src/android/JPushPlugin.java
@@ -19,7 +19,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import java.util.Map.Entry;
 
-import com.ljg.push.R;
+import __PACKAGE_NAME__.R;
 
 import cn.jpush.android.api.BasicPushNotificationBuilder;
 import cn.jpush.android.api.CustomPushNotificationBuilder;
