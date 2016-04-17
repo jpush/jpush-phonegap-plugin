560e1bb9bd31d938b13813e4daa89ad2b376805b
Hevin <328066446@qq.com>
2016-04-17 18:11:43 +0800

    Optimise code

--- src/android/JPushPlugin.java
+++ src/android/JPushPlugin.java
@@ -21,6 +21,7 @@ import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -428,7 +429,6 @@ public class JPushPlugin extends CordovaPlugin {
                 cordovaActivity, R.layout.test_notification_layout,
                 R.id.icon, R.id.title, R.id.text);
         builder.developerArg0 = "Custom Builder 1";
-        builder.layoutIconDrawable = R.drawable.jpush_notification_icon;
         JPushInterface.setPushNotificationBuilder(2, builder);
         JSONObject obj = new JSONObject();
         try {
